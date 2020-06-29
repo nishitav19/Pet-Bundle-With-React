f00f603d353167176b1e47813e2b6e07ccdaf015
Nishita V <nishitavaddem@gmail.com>
2020-06-29 17:36:53 +0800

    Hosting

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -2,15 +2,15 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 
 var firebaseConfig = {
-    apiKey: "AIzaSyB-vBhL6eHt6IdLUFFy13VS-KDAWJd6Su8",
-    authDomain: "mario-ff5a4.firebaseapp.com",
-    databaseURL: "https://mario-ff5a4.firebaseio.com",
-    projectId: "mario-ff5a4",
-    storageBucket: "mario-ff5a4.appspot.com",
-    messagingSenderId: "711642467896",
-    appId: "1:711642467896:web:c909e144bc5e5c801820cd",
-    measurementId: "G-5RNHM1MW7D"
+    apiKey: "AIzaSyCs69teMtVUzz3zb0wGVkgsjSNZI9I0w04",
+    authDomain: "pet-bundle-1.firebaseapp.com",
+    databaseURL: "https://pet-bundle-1.firebaseio.com",
+    projectId: "pet-bundle-1",
+    storageBucket: "pet-bundle-1.appspot.com",
+    messagingSenderId: "492427483620",
+    appId: "1:492427483620:web:4a49f19155ec8b5c06cff1"
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.firestore()
